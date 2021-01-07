ed84fabcb39750209fc41808d5a92cbbbbdd0aaa
takatsugu-kato <t.kato@crestecusa.com>
2021-01-07 14:42:44 +0900

    add install_requires

--- setup.py
+++ setup.py
@@ -14,5 +14,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7.3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'retry',
+        'urllib3',
+        'lxml'
     ]
 )
