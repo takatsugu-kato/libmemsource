9d576795147da35fa066cb105ac3661250dab097
takatsugu-kato <t.kato@crestecusa.com>
2022-04-28 14:12:22 +0900

    change ver.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="libmemsource",
-    version="1.4.8",
+    version="1.4.9",
     author="tkato",
     author_email="kato@ideainstitute.co.jp",
     description="libmemsource module handles memsource.",
